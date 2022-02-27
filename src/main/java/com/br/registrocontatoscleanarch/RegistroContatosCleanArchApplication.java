347adb227fc261266f070818d99fa77282886643
Vanderlei Loureiro <vanderloureiro.dev@gmail.com>
2022-02-27 01:44:51 -0300

    primeiro commit

---
+++ src/main/java/com/br/registrocontatoscleanarch/RegistroContatosCleanArchApplication.java
@@ -0,0 +1,13 @@
+package com.br.registrocontatoscleanarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegistroContatosCleanArchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RegistroContatosCleanArchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/br/registrocontatoscleanarch/RegistroContatosCleanArchApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.registrocontatoscleanarch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RegistroContatosCleanArchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
