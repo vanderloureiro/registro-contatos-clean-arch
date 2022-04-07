3883a426a056d7a4161034fc698c930167b1a29e
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-04-07 18:26:01 -0300

    Teste de BuscarContatosUserCase

--- src/test/java/com/br/registrocontatoscleanarch/core/usercases/BuscarContatosUserCaseTest.java
+++ src/test/java/com/br/registrocontatoscleanarch/core/usercases/BuscarContatosUserCaseTest.java
@@ -2,8 +2,11 @@ package com.br.registrocontatoscleanarch.core.usercases;
 
 import com.br.registrocontatoscleanarch.core.domain.Contato;
 import com.br.registrocontatoscleanarch.infrastructure.repository.ContatoRepositoryAdapter;
+import org.assertj.core.api.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -15,6 +18,7 @@ import java.util.List;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class BuscarContatosUserCaseTest {
 
     @Mock
@@ -23,17 +27,20 @@ public class BuscarContatosUserCaseTest {
     @InjectMocks
     private BuscarContatosUserCase buscarContatosUserCase;
 
+    private List<Contato> listaContatos;
+
     @BeforeAll
-    private void setup() {}
+    public void setup() {
+        Contato contato = Contato.builder().build();
+        this.listaContatos = Arrays.asList(contato);
+    }
 
     @Test
     public void sucesso_busca_todos_contatos() {
-        when(contatoRepository.buscaTodos()).thenReturn(retornaListaContatos());
-        buscarContatosUserCase.buscar();
+        when(contatoRepository.buscaTodos()).thenReturn(listaContatos);
+        List<Contato> resultado = buscarContatosUserCase.buscar();
+        Assertions.assertEquals(1, resultado.size());
     }
 
-    private List<Contato> retornaListaContatos() {
-        Contato contato = Contato.builder().build();
-        return Arrays.asList(contato);
-    }
 }
+
