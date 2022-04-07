d7295c681a4b05e31aa0141677bec65a9ac4dae0
Vanderlei Loureiro <vanderlei.loureiro@evoluum.com.br>
2022-04-07 18:41:57 -0300

    Correcao de verificacao de regra de negócio e teste de SalvarContatoUserCase

--- src/main/java/com/br/registrocontatoscleanarch/core/usercases/SalvarContatoUserCase.java
+++ src/main/java/com/br/registrocontatoscleanarch/core/usercases/SalvarContatoUserCase.java
@@ -22,17 +22,17 @@ public class SalvarContatoUserCase {
     }
 
     private void verificaSeEmailValido(String email) {
-        if (email != null && !email.isEmpty() && !email.contains("@")) {
-            log.error("E-mail {} não é valido", email);
-            throw new AtributoInvalidoException("E-mail não valido");
-        }
+        if (email != null && email.contains("@")) { return; }
+
+        log.error("E-mail {} não é valido", email);
+        throw new AtributoInvalidoException("E-mail não valido");
     }
 
     private void verificaSeTelefoneValido(String telefone) {
-        if (telefone != null && !telefone.isEmpty() && telefone.length() != 8) {
-            log.error("Telefone {} não é valido. Deve conter 8 números", telefone);
-            throw new AtributoInvalidoException("Telefone não é valido. Deve conter DDD + 8 números");
-        }
+        if (telefone != null && telefone.length() == 8) {return;}
+
+        log.error("Telefone {} não é valido. Deve conter 8 números", telefone);
+        throw new AtributoInvalidoException("Telefone não é valido. Deve conter 8 números");
     }
 
 }
--- src/test/java/com/br/registrocontatoscleanarch/core/usercases/SalvarContatoUserCaseTest.java
+++ src/test/java/com/br/registrocontatoscleanarch/core/usercases/SalvarContatoUserCaseTest.java
@@ -1,19 +1,56 @@
 package com.br.registrocontatoscleanarch.core.usercases;
 
-import com.br.registrocontatoscleanarch.infrastructure.repository.jparepositories.ContatoJpaRepository;
+import com.br.registrocontatoscleanarch.core.domain.Contato;
+import com.br.registrocontatoscleanarch.core.exceptions.AtributoInvalidoException;
+import com.br.registrocontatoscleanarch.core.ports.ContatoRepository;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 @ExtendWith(MockitoExtension.class)
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class SalvarContatoUserCaseTest {
 
     @Mock
-    private ContatoJpaRepository contatoJpaRepository;
+    private ContatoRepository contatoRepository;
 
     @InjectMocks
     private SalvarContatoUserCase salvarContatoUserCase;
+
+    @Test
+    public void successo_salvar_contato_teste() {
+        Contato contato = Contato.builder()
+                .telefone("40028922")
+                .email("teste@email.com")
+                .nome("fulano")
+                .build();
+        salvarContatoUserCase.salvar(contato);
+    }
+
+    @Test
+    public void excecao_email_invalido_teste() {
+        Contato contato = Contato.builder()
+                .telefone("40028922")
+                .email("teste")
+                .nome("fulano")
+                .build();
+
+        assertThrows(AtributoInvalidoException.class, () -> salvarContatoUserCase.salvar(contato));
+    }
+
+    @Test
+    public void excecao_telefone_invalido_teste() {
+        Contato contato = Contato.builder()
+                .telefone("00")
+                .email("teste@email.com")
+                .nome("fulano")
+                .build();
+
+        assertThrows(AtributoInvalidoException.class, () -> salvarContatoUserCase.salvar(contato));
+    }
 }
